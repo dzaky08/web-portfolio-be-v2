58e620e86a6c1dd463b6757f283cd9b5638e3254
dzaky 09 <dzaky08@gmail.com>
2025-11-29 19:50:08 +0700

    add app.get with url (/) in api/server.js

--- api/server.js
+++ api/server.js
@@ -1,26 +1,29 @@
 import express from "express";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import db from '../config/Database.js';
+import db from "../config/Database.js";
 import router from "../routes/index.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000; // <--- DEFINISI PORT
 
-try {     
-    await db.authenticate();
-    console.log('Database connected');
-    // await db.sync(); 
-
-    // --- BARIS WAJIB UNTUK MENJALANKAN SERVER ---
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
-    // ----------------------------------------------
+try {
+  await db.authenticate();
+  console.log("Database connected"); // await db.sync();
+  // --- BARIS WAJIB UNTUK MENJALANKAN SERVER ---
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+  // ----------------------------------------------
+  app.get("/", (req, res) => {
+    res
+      .status(200)
+      .json({ message: "Welcome to Portfolio API v2. Server is healthy." });
+  });
 } catch (error) {
-    console.error('Error saat koneksi atau menjalankan server:', error);
-    process.exit(1); // Hentikan proses jika gagal
+  console.error("Error saat koneksi atau menjalankan server:", error);
+  process.exit(1); // Hentikan proses jika gagal
 }
 
 app.use(cookieParser());
